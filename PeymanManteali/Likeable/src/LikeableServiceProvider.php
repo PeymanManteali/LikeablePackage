3653f70a5c8ac65663ec0ce8051c734bd40d3682
peyman <peyman.mantali@gmail.com>
2024-01-16 10:09:00 +0330

    add license

--- PeymanManteali/Likeable/src/LikeableServiceProvider.php
+++ PeymanManteali/Likeable/src/LikeableServiceProvider.php
@@ -20,9 +20,10 @@ class LikeableServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        dd(__DIR__ . '/migrations');
         $this->loadMigrationsFrom(__DIR__ . '/migrations');
         $this->publishes([
-            realpath(__DIR__ . '/../migrations')=>database_path('migrations')
+            realpath(__DIR__ . '/migrations')=>database_path('migrations')
         ], 'migrations');
 
     }
